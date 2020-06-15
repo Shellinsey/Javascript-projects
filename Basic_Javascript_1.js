2cfda0b4521d6eaa804adb7cacd93629fb811bf6
Shellinsey <59896903+Shellinsey@users.noreply.github.com>
2020-06-15 11:44:44 -0700

    Add files via upload

---
+++ Basic_Javascript_1.js
@@ -0,0 +1,12 @@
+document.write ("\"Be who are and say what you feel,"
++"because those who mind don\'t matter and those that matter don\'t mind.\""
++"-Dr. Seuss");
+
+var B=" Great"+"quotes" 
+var B=B.fontcolor("green") //defining a color to a variable
+document.write (B)
+
+var Animals="African", Monkey="Bob", Elephant="George", tiger="Milly", Hippo="Abby";
+document.write (Elephant);
+var Animals=Animals.fontcolor("Pink")
+document.write (4*8);=]
