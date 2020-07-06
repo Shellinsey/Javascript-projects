7e43510eadffa06ac3737bd2ce4c06a78c78eadf
Shellinsey <59896903+Shellinsey@users.noreply.github.com>
2020-07-06 12:06:23 -0700

    Add files via upload

--- Basic_JavaScript_5.js
+++ Basic_JavaScript_5.js
@@ -1,4 +1,17 @@
-// JavaScript source code
-document.write (typeof "Word");
 
-document.write (typeof 3);
+	function not_Function (){
+		document.getElementById("Not").innerHTML = !(20<10);
+}
+
+
+		x=12;
+		y=13;
+		function my_Function (){
+			document.write (x===y);
+		}
+		
+		m=82;
+		L="82";
+		function my_Function (){
+			document.write (m ===L);
+		}
